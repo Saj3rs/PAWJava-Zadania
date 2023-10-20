6d9e2a096ac7a26c9e760cdfc6b58ea2ea392496
Saj3rs <damfig@interia.eu>
2023-10-20 21:33:26 +0200

    Basic working Credit Calculator

--- jsf_02/src/main/java/com/jsfcourse/calc/CalcBB.java
+++ jsf_02/src/main/java/com/jsfcourse/calc/CalcBB.java
@@ -1,58 +1,112 @@
 package com.jsfcourse.calc;
 
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
-import jakarta.inject.Inject;
-import jakarta.inject.Named;
 
 @Named
 @RequestScoped
+//@SessionScoped
 public class CalcBB {
-	private String x;
-	private String y;
-	private Double result;
+	private String amm;
+	private String years;
+	private Double full;
+	private String interest;
+	private Double monthly;
+	
+	
+
+	
 	
 	@Inject
 	FacesContext ctx;
+
+	public String getAmm() {
+		return amm;
+	}
+
+	public void setAmm(String amm) {
+		this.amm = amm;
+	}
 	
-	public String getX() {
-		return x;
+	public String getYears() {
+		return years;
 	}
 
-	public void setX(String x) {
-		this.x = x;
+	public void setYears(String years) {
+		this.years = years;
+	}
+	
+	public String getInterest() {
+		return interest;
 	}
 
-	public String getY() {
-		return y;
+	public void setInterest(String interest) {
+		this.interest = interest;
 	}
 
-	public void setY(String y) {
-		this.y = y;
+	public Double getFull() {
+		return full;
 	}
 
-	public Double getResult() {
-		return result;
+	public void setFull(Double full) {
+		this.full = full;
 	}
 
-	public String calc() {		
+	public Double getMonthly() {
+		return monthly;
+	}
+
+	public void setMonthly(Double monthly) {
+		this.monthly = monthly;
+	}
+
+	public boolean doTheMath() {
 		try {
-			double x = Double.parseDouble(this.x);
-			double y = Double.parseDouble(this.y);
 			
-			result = x + y;
+			double amm = Double.parseDouble(this.amm);
+			double years = Double.parseDouble(this.years);
+			double interest = Double.parseDouble(this.interest)/100;
+			double mYear=years;
+			full=amm;
+			while(years>0){
+	            full +=interest*full;
+	            years--;
+	        }
+	        //obliczenie kwoty miesięcznej
 			
-			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
-			return "showresult"; 
+	        setMonthly(full/(12*mYear));
+			
+
+			//ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
 		} catch (Exception e) {
-			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
-			return null; 
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów ", null));
+			return false;
 		}
-				
+	}
+
+	// Go to "showresult" if ok
+	public String calc() {
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+	}
+
+	// Put result in messages on AJAX call
+	public String calc_AJAX() {
+		if (doTheMath()) {
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Pelna suma do splacenia: " + full, null));
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Miesieczna oplata: " + monthly, null));
+		}
+		return null;
 	}
 
 	public String info() {
-		return "info"; 
+		return "info";
 	}
 }
